ffef83970444d477282494916a6266c02651977c
Anjesh Tuladhar <anjeshtuladhar@gmail.com>
2015-06-11 14:23:06 +0545

    catch exception for failed downloads

--- nrgi-test/testnrgi.py
+++ nrgi-test/testnrgi.py
@@ -22,13 +22,17 @@ with open("nrgi-pdf-list.txt") as pdfLinksList:
     for pdfLink in pdfLinksList:
         pdfName = os.path.basename(urlparse.urlsplit(pdfLink.strip()).path)
         outfile = os.path.join('pdf', pdfName)
-        if not os.path.isfile(outfile):
-            print "downloading ", pdfLink
-            download_file(pdfLink.strip(), outfile)
-        pdfProcessor = PDFProcessor(outfile, 'out')
-        isStructured = "Structured" if pdfProcessor.isStructured() else "Scanned"
-        print pdfName, ":", isStructured
-        csvFile.writerow([pdfName, isStructured])
+        try:
+            if not os.path.isfile(outfile):
+                print "downloading ", pdfLink
+                download_file(pdfLink.strip(), outfile)
+        except urllib2.HTTPError, e:
+            status = "download failed " + str(e.code)
+        else:
+            pdfProcessor = PDFProcessor(outfile, 'out')
+            status = "Structured" if pdfProcessor.isStructured() else "Scanned"
+        print pdfName, ":", status
+        csvFile.writerow([pdfName, status])
 statusFile.close()        
 
 
