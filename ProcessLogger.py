4ab1bae56650bc18c4cca9ab8a6a86ec68942dd3
Anjesh Tuladhar <anjeshtuladhar@gmail.com>
2015-06-13 13:46:00 +0545

    use absolute path for log file

--- ProcessLogger.py
+++ ProcessLogger.py
@@ -6,13 +6,16 @@ import sys
 LOG_DIR = 'logs'
 
 def getLogger(loggername, filename = 'processing.log'):
-  if not os.path.exists(LOG_DIR):
-      os.makedirs(LOG_DIR)
+  scriptPath = os.path.dirname(os.path.abspath(__file__))
+
+  if not os.path.exists(os.path.join(scriptPath, LOG_DIR)):
+      os.makedirs(os.path.join(scriptPath, LOG_DIR))
+  logfile = os.path.join(scriptPath, LOG_DIR, filename)
 
   logger = logging.getLogger(loggername)
   logger.setLevel(logging.DEBUG)
 
-  handler = logging.FileHandler(os.path.join(LOG_DIR, filename))
+  handler = logging.FileHandler(logfile)
   handler.setLevel(logging.DEBUG)
 
   consolehandler = logging.StreamHandler()
